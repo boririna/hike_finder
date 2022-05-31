23ca8082cf40f00a0dd4caf7b263649103d6352f
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-31 10:47:27 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :hikes do
+    resources :likes, only: [:create, :update]
+    resources :reviews, only: [:create, :update]
+  end
+  resources :likes, only: [:index,:show,:destroy]
+  resources :reviews, only: [:index,:show,:destroy]
 end
