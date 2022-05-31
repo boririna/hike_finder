2117cba9ebc7305b16a8b58f1e2ba6b57c7b01cc
Giannina Binder <giannina@haldenweg.ch>
2022-05-31 10:31:00 +0200

    added the reviews controller, routes and added validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :hike
+
+  validates :user, uniqueness: { scope: :hike }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :hike
+
+  validates :rating, :content, presence: true
+  validates :content, length: { minimum: 10 }
 end
