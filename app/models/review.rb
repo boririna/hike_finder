d1b5736b3f2e2e51306c34f3d227e819a5f24c1e
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-31 14:00:52 +0200

    added average method on review model + added hikes show view

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,9 @@ class Review < ApplicationRecord
 
   validates :rating, :content, presence: true
   validates :content, length: { minimum: 10 }
+
+  def average_rating(hike)
+    Review.where(hike_id: hike.id).average(:rating).to_i
+  end
+
 end
