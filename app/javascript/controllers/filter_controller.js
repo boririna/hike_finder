e348db1b12b0a83e08be179d282d0b1cf1fe7915
Cristina Sult <sult_cristina@yahoo.com>
2022-06-02 11:48:20 +0200

    good range with updated number

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["text", "form"];
+  static targets = ["text", "form", "length", "altitude", "lengthLabel", "altitudeLabel"];
 
   displayForm() {
     this.textTarget.classList.add("d-none")
@@ -11,6 +11,16 @@ export default class extends Controller {
   hideForm() {
     this.textTarget.classList.remove("d-none")
     this.formTarget.classList.add("d-none")
+  }
+
+  updateLength(e) {
+    this.lengthTarget.value = e.target.value
+    this.lengthLabelTarget.innerHTML = "Length: " + e.target.value +"km"
+  }
+
+  updateAltitude(e) {
+    this.altitudeTarget.value = e.target.value
+    this.altitudeLabelTarget.innerHTML = "Altitude gain: " + e.target.value +"m"
   }
 
 }
