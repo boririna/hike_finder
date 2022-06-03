c01d77d957c19d26dea42ee3734c8ae5841a79a1
Cristina Sult <sult_cristina@yahoo.com>
2022-06-03 10:39:24 +0200

    added the time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_084726) do
+ActiveRecord::Schema.define(version: 2022_06_03_083238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_06_01_084726) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "hiking_time"
   end
 
   create_table "likes", force: :cascade do |t|
